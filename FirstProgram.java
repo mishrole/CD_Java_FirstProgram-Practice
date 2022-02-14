667dda710ba5ed79b5dba93fc5ab42974c6f3d4d
Mitchell Rodríguez <mitrodle@gmail.com>
2022-02-14 11:08:47 -0500

    add: First Program (Practice)

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        String name = "Mitchell";
+        Integer age = 24;
+        String city = "Lima";
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("My city is " + city);
+    }
+}
